4036ca67b2ddb8f3f43c216dd04a4c45811c85c9
Mila Hose <mila.hose@gmail.com>
2017-09-07 17:44:58 -0700

    Add final server

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 var http = require('http');
+var fs = require('fs');
+var html;
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
+
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    var obj = {
+      firstName: 'Jane', 
+      lastName: 'Doe'
+    };
+
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404);
+  res.end();
+  }
+
 }).listen(3000);
